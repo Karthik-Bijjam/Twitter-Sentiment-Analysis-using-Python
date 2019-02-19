a004981748929e7ceb8309d41cb1e5096ee113e7
karthik <s533488@nwmissouri.edu>
2019-02-19 14:52:53 -0600

    Sentiment Analysis using python updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Twitter-Sentiment-Analysis-using-Python
+﻿# Twitter-Sentiment-Analysis-using-Python
 Sentiment Analysis is the process of ‘computationally’ determining whether a piece of writing is positive, negative or neutral. It’s also known as opinion mining, deriving the opinion or attitude of a speaker.
+
+Here, I have used CNN, Netflix and Barack Obama Pickle for Sentiment Analysis.
+Pickle which are used for serializing and de-serializing a Python object structure. Any object in python can be pickled so that it can be saved on disk. 
---
+++ sentimentAnalysisMethod.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 21:45:24 2019
+
+@author: S533488
+"""
+
+
+
+def sentimentAnalysisFunction(TweetsData):
+    from textblob import TextBlob
+    import sys, tweepy
+    import matplotlib.pyplot as plt
+    import pickle
+    import numpy as np
+    import datetime
+    from collections import Counter
+    
+    def percentage(part, whole):
+        return 100*float(part)/float(whole);
+    
+    positive = 0
+    negative = 0
+    polarity = 0
+    neutral = 0
+    count = 0
+
+    for  tweet in TweetsData:
+        analysis = TextBlob(tweet.text)
+        polarity += analysis.sentiment.polarity
+        count = count + 1
+        if(analysis.sentiment.polarity < 0.00):
+            negative += 1
+        elif(analysis.sentiment.polarity == 0):
+            neutral += 1
+        elif(analysis.sentiment.polarity > 0.00):
+            positive += 1
+        
+        
+
+    positivePercentage = percentage(positive, count)
+    negativePercentage = percentage(negative, count)
+    neutralPercentage = percentage(neutral, count)
+
+    positive = format(positivePercentage,'.2f')
+    negative = format(negativePercentage,'.2f')
+    neutral = format(neutralPercentage,'.2f')
+    
+
+    labels = ['Positive '+str(positive)+'%','Neutral:' +str(neutral)+'%','Negative:' +str(negative)+'%']
+    sizes = [positive,neutral,negative]
+    colors = ['yellowgreen','gold','red']
+    patches,texts = plt.pie(sizes, colors = colors, startangle = 90)
+    plt.legend (patches,labels,loc = 'best')
+    plt.title('How people are reacting on Tweets by analyzing ' + str(count) +' Tweets')
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
